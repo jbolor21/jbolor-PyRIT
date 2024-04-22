67118c2fdaadfbf081d972bce31bbb410c485c62
Bolor <bjagdagdorj@gmail.com>
2024-04-22 11:03:25 -0700

    merging changes initial commit

--- pyrit/prompt_converter/__init__.py
+++ pyrit/prompt_converter/__init__.py
@@ -12,7 +12,6 @@ from pyrit.prompt_converter.unicode_confusable_converter import UnicodeConfusabl
 from pyrit.prompt_converter.unicode_sub_converter import UnicodeSubstitutionConverter
 from pyrit.prompt_converter.variation_converter import VariationConverter
 from pyrit.prompt_converter.random_capital_letters_converter import RandomCapitalLettersConverter
-from pyrit.prompt_converter.text_to_audio_converter import TextToAudio
 
 
 __all__ = [
@@ -26,5 +25,4 @@ __all__ = [
     "UnicodeSubstitutionConverter",
     "VariationConverter",
     "RandomCapitalLettersConverter",
-    "TextToAudio",
 ]
